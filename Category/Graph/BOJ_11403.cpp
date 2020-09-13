f77d64a75b6e521cd8f2b05efc797c2322981ad3
kkmin223 <rkdals133@gmail.com>
2020-09-13 10:36:18 +0000

    BOJ_11403 upload

---
+++ Category/Graph/BOJ_11403.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int n;
+  int graph[100][100]={0,};
+  cin >> n;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      cin>> graph[i][j];
+    }
+  }
+  for(int k=0;k<n;k++){
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        if(graph[i][k]&&graph[k][j]){
+          graph[i][j]=1;
+        }
+      }
+    }
+  }
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      cout<< graph[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+}
